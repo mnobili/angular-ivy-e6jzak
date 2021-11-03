17a1ba14eb385bb00ad206737d3e0895711513ba
mnobili <milosh.nobili@gmail.com>
2021-11-03 12:01:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-f3uzdo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-f3uzdo)
---
+++ src/app/first/first.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+  @Input() companyName = "Agos"
+  changeCompanyName() {
+    this.companyName = "This Binding works!"
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
